987fd12899b05928a3229e521f7cb39d1f854fc1
venkatping <chilakasainaveen@gmail.com>
2020-07-06 04:28:50 +0000

    few more section11

--- section11/get_files_from_directory.py
+++ section11/get_files_from_directory.py
@@ -1,10 +1,20 @@
 #!/usr/local/bin/python3
 import os
 req_path=input("Enter your directory path: ")
-req_ext=input("Enter the required extension: ")
+#req_ext=input("Enter the required extension: ")
 if os.path.isfile(req_path):
     print(f"The Given path: {path} is a file")
 else:
     all_files_dirs=os.listdir(req_path)
     if len(all_files_dirs)==0:
-        print(f"The Given path is empty")
+        print(f"The required path is empty")
+    else:
+        req_ext=input("Enter the required files extension: ")
+        req_files=[]
+        for each in all_files_dirs:
+            if each.endswith(req_ext):
+                req_files.append(each)
+        if len(req_files)==0:
+            print(f"Ther is no files with required extension {req_ext}")
+        else:
+            print(f"The required files are {req_files}")
