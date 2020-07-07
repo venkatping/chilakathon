1a79378905ab40b288282b88605d12a0d7100c3d
venkatping <chilakasainaveen@gmail.com>
2020-07-07 01:23:46 +0000

    few more from section11

---
+++ section11/loop_control_statements.py
@@ -0,0 +1,57 @@
+#!/usr/local/bin/python3
+"""
+print("before loop")
+for each in [2,4,6,8,10,12,14]:
+	print(each)
+	break
+print("after loop")
+"""
+"""
+print("before loop")
+for each in [2,4,6,8,10,12,14]:
+ break
+ print(each)
+
+print("after loop")
+"""
+"""
+print("before loop")
+for each in [2,4,6,8,10,12,14]:
+ if each==6:
+  continue
+ print(each)
+print("after loop")
+"""
+"""
+paths=['/usr/bin','/usr/bin/httpd','/etc/httpd/httpd_config']
+for each in paths:
+  if 'httpd' in each:
+   print(each)
+"""
+"""
+value=4
+while True:
+    if value==34:
+        break
+    print(value)
+    value=value+10
+"""
+"""
+for each in range(10):
+    if each==5:
+        continue
+    print(each)
+"""
+"""
+if True:
+    pass
+"""
+"""
+for each in range(10):
+    pass
+"""
+
+
+
+
+
